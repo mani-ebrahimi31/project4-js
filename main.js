aa8b5c7bc4d9eafc89823d8197aaf08a48c6bb62
mani-ebrahimi31 <maniebrahimi31@gmail.com>
2021-10-16 00:53:49 +0330

    chenge code for main file

--- main.js
+++ main.js
@@ -2,11 +2,9 @@ let array1 = [1, 2, 3, 3, 4, 6, 6, 7, 8];
 let array2 = [0, 0, 1, 5, 5, 5, 6, 8, 10];
 
 //your code
-
 var duplicates = array1.filter(function (val) {
   return array2.indexOf(val) != -1;
 });
 
 //output
-
 console.log(duplicates);
